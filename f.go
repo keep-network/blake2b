04b7789d5c6a323c92a5baf6d1ef8039cd6cb404
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-07-01 14:11:50 +0200

    Pulled in Blake2B checksum code from golang.org/x/crypto
    The code and test vectors of Blake2B checksum have been pulled in to
    this library. It will allow us to define precompiles for Blake2b
    checksums, if needed. Additionally, we can now test against vectors from
    RFC instead of autogenerating vectors just for F.
    
    I have also modified F implementation removing the loop over block
    content. According to the RFC, this loop is not a part of F function -
    see `blake2b_update` C function in the RFC for reference.

--- f.go
+++ f.go
@@ -4,24 +4,14 @@
 //
 // Modified by KEEP SEZC to expose F compression function.
 //
-package blake2
+
+package blake2b
 
 import (
-	"encoding/binary"
 	"math/bits"
 )
 
-const (
-	// BlockSize is the blocksize of BLAKE2 in bytes.
-	BlockSize = 128
-)
-
-var iv = [8]uint64{
-	0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
-	0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179,
-}
-
-// the precomputed values for the first 10 rounds of BLAKE2
+// the precomputed values for BLAKE2b
 // there are 10 16-byte arrays - one for each round
 // the entries are calculated from the sigma constants.
 var precomputed = [10][16]byte{
@@ -37,163 +27,149 @@ var precomputed = [10][16]byte{
 	{10, 8, 7, 1, 2, 4, 6, 5, 15, 9, 3, 13, 11, 14, 12, 0},
 }
 
-// F is a compression function for Blake2. It takes as an argument the state
-// vector `h`, message block vector `mb`, offset counter `t`, final
+// F is a compression function for BLAKE2b. It takes as an argument the state
+// vector `h`, message block vector `m`, offset counter `t`, final
 // block indicator flag `f`, and number of rounds `rounds`. The state vector
 // provided as the first parameter is modified by the function.
-func F(h *[8]uint64, mb [BlockSize]byte, t [2]uint64, f bool, rounds uint32) {
-	var m [16]uint64
-	t0, t1 := t[0], t[1]
+func F(h *[8]uint64, m [16]uint64, c [2]uint64, f bool, rounds uint32) {
+	c0, c1 := c[0], c[1]
 
-	for i := 0; i < len(mb); {
-		t0 += BlockSize
-		if t0 < BlockSize {
-			t1++
-		}
+	v0, v1, v2, v3, v4, v5, v6, v7 := h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7]
+	v8, v9, v10, v11, v12, v13, v14, v15 := iv[0], iv[1], iv[2], iv[3], iv[4], iv[5], iv[6], iv[7]
+	v12 ^= c0
+	v13 ^= c1
 
-		v0, v1, v2, v3, v4, v5, v6, v7 := h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7]
-		v8, v9, v10, v11, v12, v13, v14, v15 := iv[0], iv[1], iv[2], iv[3], iv[4], iv[5], iv[6], iv[7]
-		v12 ^= t0
-		v13 ^= t1
-
-		if f {
-			v14 ^= 0xffffffffffffffff
-		}
-
-		for j := range m {
-			m[j] = binary.LittleEndian.Uint64(mb[i:])
-			i += 8
-		}
-
-		for j := uint32(0); j < rounds; j++ {
-			s := &(precomputed[j%10])
-
-			v0 += m[s[0]]
-			v0 += v4
-			v12 ^= v0
-			v12 = bits.RotateLeft64(v12, -32)
-			v8 += v12
-			v4 ^= v8
-			v4 = bits.RotateLeft64(v4, -24)
-			v1 += m[s[1]]
-			v1 += v5
-			v13 ^= v1
-			v13 = bits.RotateLeft64(v13, -32)
-			v9 += v13
-			v5 ^= v9
-			v5 = bits.RotateLeft64(v5, -24)
-			v2 += m[s[2]]
-			v2 += v6
-			v14 ^= v2
-			v14 = bits.RotateLeft64(v14, -32)
-			v10 += v14
-			v6 ^= v10
-			v6 = bits.RotateLeft64(v6, -24)
-			v3 += m[s[3]]
-			v3 += v7
-			v15 ^= v3
-			v15 = bits.RotateLeft64(v15, -32)
-			v11 += v15
-			v7 ^= v11
-			v7 = bits.RotateLeft64(v7, -24)
-
-			v0 += m[s[4]]
-			v0 += v4
-			v12 ^= v0
-			v12 = bits.RotateLeft64(v12, -16)
-			v8 += v12
-			v4 ^= v8
-			v4 = bits.RotateLeft64(v4, -63)
-			v1 += m[s[5]]
-			v1 += v5
-			v13 ^= v1
-			v13 = bits.RotateLeft64(v13, -16)
-			v9 += v13
-			v5 ^= v9
-			v5 = bits.RotateLeft64(v5, -63)
-			v2 += m[s[6]]
-			v2 += v6
-			v14 ^= v2
-			v14 = bits.RotateLeft64(v14, -16)
-			v10 += v14
-			v6 ^= v10
-			v6 = bits.RotateLeft64(v6, -63)
-			v3 += m[s[7]]
-			v3 += v7
-			v15 ^= v3
-			v15 = bits.RotateLeft64(v15, -16)
-			v11 += v15
-			v7 ^= v11
-			v7 = bits.RotateLeft64(v7, -63)
-
-			v0 += m[s[8]]
-			v0 += v5
-			v15 ^= v0
-			v15 = bits.RotateLeft64(v15, -32)
-			v10 += v15
-			v5 ^= v10
-			v5 = bits.RotateLeft64(v5, -24)
-			v1 += m[s[9]]
-			v1 += v6
-			v12 ^= v1
-			v12 = bits.RotateLeft64(v12, -32)
-			v11 += v12
-			v6 ^= v11
-			v6 = bits.RotateLeft64(v6, -24)
-			v2 += m[s[10]]
-			v2 += v7
-			v13 ^= v2
-			v13 = bits.RotateLeft64(v13, -32)
-			v8 += v13
-			v7 ^= v8
-			v7 = bits.RotateLeft64(v7, -24)
-			v3 += m[s[11]]
-			v3 += v4
-			v14 ^= v3
-			v14 = bits.RotateLeft64(v14, -32)
-			v9 += v14
-			v4 ^= v9
-			v4 = bits.RotateLeft64(v4, -24)
-
-			v0 += m[s[12]]
-			v0 += v5
-			v15 ^= v0
-			v15 = bits.RotateLeft64(v15, -16)
-			v10 += v15
-			v5 ^= v10
-			v5 = bits.RotateLeft64(v5, -63)
-			v1 += m[s[13]]
-			v1 += v6
-			v12 ^= v1
-			v12 = bits.RotateLeft64(v12, -16)
-			v11 += v12
-			v6 ^= v11
-			v6 = bits.RotateLeft64(v6, -63)
-			v2 += m[s[14]]
-			v2 += v7
-			v13 ^= v2
-			v13 = bits.RotateLeft64(v13, -16)
-			v8 += v13
-			v7 ^= v8
-			v7 = bits.RotateLeft64(v7, -63)
-			v3 += m[s[15]]
-			v3 += v4
-			v14 ^= v3
-			v14 = bits.RotateLeft64(v14, -16)
-			v9 += v14
-			v4 ^= v9
-			v4 = bits.RotateLeft64(v4, -63)
-
-		}
-
-		h[0] ^= v0 ^ v8
-		h[1] ^= v1 ^ v9
-		h[2] ^= v2 ^ v10
-		h[3] ^= v3 ^ v11
-		h[4] ^= v4 ^ v12
-		h[5] ^= v5 ^ v13
-		h[6] ^= v6 ^ v14
-		h[7] ^= v7 ^ v15
+	if f {
+		v14 ^= 0xffffffffffffffff
 	}
-	t[0], t[1] = t0, t1
+
+	for j := uint32(0); j < rounds; j++ {
+		s := &(precomputed[j%10])
+
+		v0 += m[s[0]]
+		v0 += v4
+		v12 ^= v0
+		v12 = bits.RotateLeft64(v12, -32)
+		v8 += v12
+		v4 ^= v8
+		v4 = bits.RotateLeft64(v4, -24)
+		v1 += m[s[1]]
+		v1 += v5
+		v13 ^= v1
+		v13 = bits.RotateLeft64(v13, -32)
+		v9 += v13
+		v5 ^= v9
+		v5 = bits.RotateLeft64(v5, -24)
+		v2 += m[s[2]]
+		v2 += v6
+		v14 ^= v2
+		v14 = bits.RotateLeft64(v14, -32)
+		v10 += v14
+		v6 ^= v10
+		v6 = bits.RotateLeft64(v6, -24)
+		v3 += m[s[3]]
+		v3 += v7
+		v15 ^= v3
+		v15 = bits.RotateLeft64(v15, -32)
+		v11 += v15
+		v7 ^= v11
+		v7 = bits.RotateLeft64(v7, -24)
+
+		v0 += m[s[4]]
+		v0 += v4
+		v12 ^= v0
+		v12 = bits.RotateLeft64(v12, -16)
+		v8 += v12
+		v4 ^= v8
+		v4 = bits.RotateLeft64(v4, -63)
+		v1 += m[s[5]]
+		v1 += v5
+		v13 ^= v1
+		v13 = bits.RotateLeft64(v13, -16)
+		v9 += v13
+		v5 ^= v9
+		v5 = bits.RotateLeft64(v5, -63)
+		v2 += m[s[6]]
+		v2 += v6
+		v14 ^= v2
+		v14 = bits.RotateLeft64(v14, -16)
+		v10 += v14
+		v6 ^= v10
+		v6 = bits.RotateLeft64(v6, -63)
+		v3 += m[s[7]]
+		v3 += v7
+		v15 ^= v3
+		v15 = bits.RotateLeft64(v15, -16)
+		v11 += v15
+		v7 ^= v11
+		v7 = bits.RotateLeft64(v7, -63)
+
+		v0 += m[s[8]]
+		v0 += v5
+		v15 ^= v0
+		v15 = bits.RotateLeft64(v15, -32)
+		v10 += v15
+		v5 ^= v10
+		v5 = bits.RotateLeft64(v5, -24)
+		v1 += m[s[9]]
+		v1 += v6
+		v12 ^= v1
+		v12 = bits.RotateLeft64(v12, -32)
+		v11 += v12
+		v6 ^= v11
+		v6 = bits.RotateLeft64(v6, -24)
+		v2 += m[s[10]]
+		v2 += v7
+		v13 ^= v2
+		v13 = bits.RotateLeft64(v13, -32)
+		v8 += v13
+		v7 ^= v8
+		v7 = bits.RotateLeft64(v7, -24)
+		v3 += m[s[11]]
+		v3 += v4
+		v14 ^= v3
+		v14 = bits.RotateLeft64(v14, -32)
+		v9 += v14
+		v4 ^= v9
+		v4 = bits.RotateLeft64(v4, -24)
+
+		v0 += m[s[12]]
+		v0 += v5
+		v15 ^= v0
+		v15 = bits.RotateLeft64(v15, -16)
+		v10 += v15
+		v5 ^= v10
+		v5 = bits.RotateLeft64(v5, -63)
+		v1 += m[s[13]]
+		v1 += v6
+		v12 ^= v1
+		v12 = bits.RotateLeft64(v12, -16)
+		v11 += v12
+		v6 ^= v11
+		v6 = bits.RotateLeft64(v6, -63)
+		v2 += m[s[14]]
+		v2 += v7
+		v13 ^= v2
+		v13 = bits.RotateLeft64(v13, -16)
+		v8 += v13
+		v7 ^= v8
+		v7 = bits.RotateLeft64(v7, -63)
+		v3 += m[s[15]]
+		v3 += v4
+		v14 ^= v3
+		v14 = bits.RotateLeft64(v14, -16)
+		v9 += v14
+		v4 ^= v9
+		v4 = bits.RotateLeft64(v4, -63)
+
+	}
+
+	h[0] ^= v0 ^ v8
+	h[1] ^= v1 ^ v9
+	h[2] ^= v2 ^ v10
+	h[3] ^= v3 ^ v11
+	h[4] ^= v4 ^ v12
+	h[5] ^= v5 ^ v13
+	h[6] ^= v6 ^ v14
+	h[7] ^= v7 ^ v15
 }
